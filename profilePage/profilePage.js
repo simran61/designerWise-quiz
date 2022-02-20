5bf34e297eb0c756384731d474669e98222c1d3b
Simran Gangwani <simrangngwani3107@gmail.com>
2022-02-21 00:30:47 +0530

    update: added profile page

---
+++ profilePage/profilePage.js
@@ -0,0 +1,16 @@
+const progressOne = document.querySelector('.progress_done_1');
+
+progressOne.style.width = progressOne.getAttribute('data-done') + '%';
+progressOne.style.opacity = 1;
+
+
+const progressTwo = document.querySelector('.progress_done_2');
+
+progressTwo.style.width = progressTwo.getAttribute('data-done') + '%';
+progressTwo.style.opacity = 1;
+
+
+const progressThree = document.querySelector('.progress_done_3');
+
+progressThree.style.width = progressThree.getAttribute('data-done') + '%';
+progressThree.style.opacity = 1;
